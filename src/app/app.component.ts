c5876dbb2fbfedaeab309b33f8e2d0dbc04f263f
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-03-25 09:11:40 -0600

    first four lessons complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
+  imports: [HomeComponent],
+  template: `<main>
+  <header class="brand-name">
+    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+  </header>
+  <section class="content">
+    <app-home></app-home>
+  </section>
+</main>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
